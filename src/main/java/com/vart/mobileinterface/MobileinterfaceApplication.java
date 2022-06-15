f608a452a5a81ab3bbcb6134368ea36dccc1391b
Vitaliy A <vitaliy.a@dev.tagsoft.co>
2022-06-15 18:56:09 +0300

    Initial commit

---
+++ src/main/java/com/vart/mobileinterface/MobileinterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.vart.mobileinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileinterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileinterfaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vart/mobileinterface/MobileinterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vart.mobileinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileinterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
