e0c6d8c9c78faa20d4bb81f23efb091acc61203f
Vitaliy A <vitaliy.a@dev.tagsoft.co>
2022-06-29 20:52:26 +0300

    Added kafka message dto

--- src/main/java/com/vart/mobileinterface/service/kafka/MessageConsumer.java
+++ src/main/java/com/vart/mobileinterface/service/kafka/MessageConsumer.java
@@ -1,6 +1,7 @@
 package com.vart.mobileinterface.service.kafka;
 
 import com.vart.mobileinterface.model.Message;
+import com.vart.mobileinterface.model.dto.ConsultationDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class MessageConsumer {
     @KafkaListener(topics = "consultation", groupId = "mobile")
-    public void listen(Message message) {
-        log.info("Received: " + message);
+    public void listen(ConsultationDto consultationDto) {
+        log.info("Received: " + consultationDto);
     }
 }
